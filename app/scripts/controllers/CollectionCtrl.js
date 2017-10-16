239dc9ba67ecc669a1ddbf544eb36ed354ca3da0
Caitlin Smallcomb <csmallcomb@gmail.com>
2017-10-15 17:23:13 -0700

    fixing after checkpoint 6 completion

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,8 @@
 (function() {
-    function CollectionCtrl() {
-        this.albums = Fixtures.getCollection(12);
+    function CollectionCtrl(Fixtures) {
+        this.albumData = Fixtures.getCollection(12);
     }
-
     angular
         .module('blocJams')
-        .controller('CollectionCtrl', CollectionCtrl);
+        .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
 })();
