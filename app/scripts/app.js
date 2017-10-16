9486fdaaa387232ba3c76872ba6b19f50ff8bf5b
Caitlin Smallcomb <csmallcomb@gmail.com>
2017-10-15 17:21:29 -0700

    Revert "Merge branch 'master' of https://github.com/csmallcomb/bloc-jams-angularjs"
    This reverts commit 2734efabdfa23956650330b7ee091556bad3eea0, reversing
    changes made to a91fbd76340d56d3429f08390060369b8754ce3a.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,6 @@
             })
             .state('album', {
                 url: '/album',
-                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
             })
             .state('collection' {
